5084bfa8b5631a04771f57abf6ac0d91be27faaa
Efthymios-Papakostopoulos <papakostopoulos.makis@hotmail.com>
2024-04-14 21:29:01 +0300

    Initial Timetable

---
+++ main.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+<?php
+include "mainPages/Header.php";
+?>
+
+<div class="container-timetable">
+    <div class="timetable">
+        <div class="week-names">
+            <div>monday</div>
+            <div>tuesday</div>
+            <div>wednesday</div>
+            <div>thursday</div>
+            <div>friday</div>
+            <div class="weekend">saturday</div>
+            <div class="weekend">sunday</div>
+        </div>
+        <div class="time-interval">
+            <div>8:00 - 10:00</div>
+            <div>10:00 - 12:00</div>
+            <div>12:00 - 14:00</div>
+            <div>14:00 - 16:00</div>
+            <div>16:00 - 18:00</div>
+            <div>18:00 - 20:00</div>
+        </div>
+        <div class="content">
+            <div>
+                <div class="accent-orange-gradient">
+                    <div>Test Lesson</div>
+                </div>
+
+            </div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div>
+                <div class="accent-green-gradient">
+                    <div>Test Lesson</div>
+                </div>
+            </div>
+            <div class="weekend"></div>
+            <div class="weekend"></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div>
+                <div class="accent-cyan-gradient"></div>
+            </div>
+            <div></div>
+            <div class="weekend"></div>
+            <div class="weekend"></div>
+            <div>
+                <div class="accent-pink-gradient"></div>
+            </div>
+            <div></div>
+            <div>
+                <div class="accent-purple-gradient"></div>
+            </div>
+            <div></div>
+            <div></div>
+            <div class="weekend"></div>
+            <div class="weekend"></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div class="weekend"></div>
+            <div class="weekend"></div>
+            <div>
+                <div class="accent-purple-gradient"></div>
+            </div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div class="weekend"></div>
+            <div class="weekend"></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div class="weekend"></div>
+            <div class="weekend"></div>
+        </div>
+    </div>
+</div>
+
+<?php
+include "mainPages/footer.php";
+
+?>
+
+
+</body>
+</html>
---
+++ mainPages/footer.php
@@ -0,0 +1,87 @@
+<div class="container-footer my-5">
+    <!-- Footer -->
+    <footer
+        class="footer text-center text-lg-start text-white"
+        style="background-color: #45526e"
+    >
+        <!-- Grid container -->
+        <div class="container p-4 pb-0">
+            <!-- Section: Links -->
+            <section class="">
+                <!--Grid row-->
+                <div class="row">
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                        <h6 class="text-uppercase mb-4 font-weight-bold">
+                            Glyndwr Timetable
+                        </h6>
+                        <p>
+                            Welcome to the timetable! For questions about the platform please contact your tutor
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <hr class="w-100 clearfix d-md-none"/>
+
+                    <!-- Grid column -->
+
+                    <hr class="w-100 clearfix d-md-none"/>
+
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                        <h6 class="text-uppercase mb-4 font-weight-bold">
+                            Useful links
+                        </h6>
+                        <p>
+                            <a class="text-white">Your Account</a>
+                        </p>
+                        <p>
+                            <a class="text-white">Become an Affiliate</a>
+                        </p>
+                        <p>
+                            <a class="text-white">Shipping Rates</a>
+                        </p>
+                        <p>
+                            <a class="text-white">Help</a>
+                        </p>
+                    </div>
+
+                    <!-- Grid column -->
+                    <hr class="w-100 clearfix d-md-none"/>
+
+                    <!-- Grid column -->
+                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                        <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+                        <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
+                        <p><i class="fas fa-envelope mr-3"></i> info@gmail.com</p>
+                        <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+                        <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+                    </div>
+                    <!-- Grid column -->
+                </div>
+                <!--Grid row-->
+            </section>
+            <!-- Section: Links -->
+
+            <hr class="my-3">
+
+            <!-- Section: Copyright -->
+            <section class="p-3 pt-0">
+                <div class="row d-flex align-items-center">
+                    <!-- Grid column -->
+                    <div class="col-md-7 col-lg-8 text-center text-md-start">
+                        <!-- Copyright -->
+                        <div class="p-3">
+                            © 2024 Copyright:
+                        </div>
+                        <!-- Copyright -->
+                    </div>
+                </div>
+            </section>
+            <!-- Section: Copyright -->
+        </div>
+        <!-- Grid container -->
+    </footer>
+    <!-- Footer -->
+</div>
+<!-- End of .container -->
