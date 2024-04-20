6800ecbebc39aba91a7d79efd9726f06c93408c2
geopiz <georgiospizanias@gmail.com>
2024-04-20 19:35:55 +0300

    title fix

--- mainPages/Header.php
+++ mainPages/Header.php
@@ -10,14 +10,14 @@ $username = isset($_SESSION['username']) ? $_SESSION['username'] : 'Guest';
 <html lang="en">
 <head>
 
-    <title>Student Timetable</title>
+    <title>Glyndwr Timetable</title>
 
 
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
     <div class="container">
-        <a class="navbar-brand" href="../main.php">Student Timetable</a>
+        <a class="navbar-brand" href="../main.php">Glyndwr Timetable</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
